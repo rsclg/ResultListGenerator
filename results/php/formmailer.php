596d8ef1737f4588be959628e4bb1ca7f443a7fc
Cliff Parnitzky <github@cliff-parnitzky.de>
2013-06-27 11:32:24 +0200

    Eventtyp im Mail Subject mit angeben

--- results/php/formmailer.php
+++ results/php/formmailer.php
@@ -8,12 +8,18 @@ while(list($name,$value)=each($_GET)) {
 	if ($name == "1_Name") {
 		$subject .= " von " . $value;
 	}
+	if ($name == "2_Veranstaltungsart") {
+		$subject .= " (" . $value . ")";
+	}
 	$message.="$name:\t\t$value\n";
 }
 
 while(list($name,$value)=each($_POST)) {
 	if ($name == "1_Name") {
 		$subject .= " von " . $value;
+	}
+	if ($name == "2_Veranstaltungsart") {
+		$subject .= " (" . $value . ")";
 	}
 	$message.="$name:\t\t$value\n";
 }
